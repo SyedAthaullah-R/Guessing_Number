dc905473b8f6a37c830917d34cfb2d7f89956b8a
Syed Athuallah R <syedathaullah786786@gmail.com>
2025-04-20 20:46:59 +0530

    Key changes to game
    Added the Game winning and loosing sounds and created a function for the that music

--- script.js
+++ script.js
@@ -1,40 +1,60 @@
 'use strict';
-let ScretNumber = Math.trunc(Math.random() * 20) + 1;
-let Score = 20;
-let highScore = 0;
+let ScretNumber, Score, highScore, audio1, audio2, audio3, playing;
+highScore = 0;
 
+function init(){
+    ScretNumber = Math.trunc(Math.random() * 20) + 1;
+    Score = 20;
+    audio1 = document.getElementById("stage");
+    audio2 = document.getElementById("winning");
+    audio3 = document.getElementById("lossing")
+    audio1.pause();
+    audio2.pause();
+    audio3.pause();
+    audio1.currentTime = 0;
+    audio2.currentTime = 0;
+    audio3.currentTime = 0;
+    playing = true; 
+}
+init();
 document.querySelector('.check').addEventListener('click',function(){
+    if(playing){
+    audio1.play();
     let guess = Number(document.querySelector('.guess').value);
 
-if(!guess){
-    document.querySelector('.message').textContent= " Please enter a number 😶‍🌫️"
-}
-else if(guess === ScretNumber){
+    if(!guess) document.querySelector('.message').textContent= " Please enter a number 😶‍🌫️"
+    
+    else if(guess === ScretNumber){
     document.querySelector('.message').textContent = " 🤩You guessed the Correct Number!"
     document.querySelector('body').style.backgroundColor = "#60b347";
     document.querySelector('.number').style.width = "40rem";
     document.querySelector('.number').textContent = ScretNumber;
+    audio3.play();
+    audio1.pause();
+    playing = false;
     if(Score > highScore){
         highScore = Score;
         document.querySelector('.highscore').textContent = highScore;
     }
 }
 else if(guess !== ScretNumber){
-   if(Score > 1) {document.querySelector('.message').textContent = guess > ScretNumber ? "↗️ Too High!" : " Too Low!";
+   if(Score > 1) {
+    document.querySelector('.message').textContent = guess > ScretNumber ? "↗️ Too High!" : " Too Low!";
     Score--;
-    document.querySelector('.score').textContent = Score;}
+    document.querySelector('.score').textContent = Score;
+}
 
 else{
     document.querySelector('.message').textContent = '😢 You Lost the game'
     document.querySelector('.score').textContent = 0;
-}}
-
-
-
+    audio2.play();
+    audio1.pause();
+}
+}
+    }
 });
 document.querySelector('.again').addEventListener('click',function(){
-    Score = 20;
-    ScretNumber = Math.trunc(Math.random() * 20) + 1;
+    init();
     document.querySelector('.message').textContent = "Start Guessing...";
     document.querySelector('.number').textContent = "?";
     document.querySelector('.score').textContent = 20;
